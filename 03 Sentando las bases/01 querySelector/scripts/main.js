4d6888a0da956795f639e5845275d5eacd35655f
luisrodolfoduarte <duarteluis2014@hotmail.com>
2020-01-13 15:12:54 -0300

    subo los ejercicios resueltos

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,44 @@
-console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+
+//El 1
+const favorito = document.querySelector(".comment");
+
+favorito.classList.add("comentarioFav");
+
+
+//Ej 2
+const linea = document.querySelectorAll("hr");
+
+for(let i = 0; i<linea.length; i++){
+    linea[i].classList.add("cambiarLinea");
+}
+
+
+//Ej 3
+
+const comentarios = document.querySelectorAll(".comment--text");
+
+
+for(let i = 0; i<comentarios.length; i++){
+
+    if(comentarios[i].innerText.length>50){
+        comentarios[i].classList.add("comment--ocultarComentario");
+    }
+
+}
+
+
+
+//Ej 4
+let contador = null;
+let mayor = 0;
+for(let x = 0; x<comentarios.length; x++){
+    
+    if(comentarios[x].innerText.length>mayor){
+        
+        mayor = comentarios[x].innerText.length;
+        contador = x;
+    }
+    
+}
+
+comentarios[contador].classList.add("comentarioMayor");
