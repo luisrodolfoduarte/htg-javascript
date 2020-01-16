2fd6b6a8575024c5f467db927ddc963b6daf2212
luisrodolfoduarte <duarteluis2014@hotmail.com>
2020-01-16 14:59:48 -0300

    subo los ejercicios resueltos

---
+++ 03 Sentando las bases/02 addEventListener/hola.md
@@ -0,0 +1,4 @@
+nombre: leo
+solucion: Crear una app donde se muestre un mapa de tu entorno, donde uno pueda identificar zonas peligrosas.
+        ademas uno pueda marcar lugares y estos lugares marcados por la gente tenga un contador
+        donde cuando llegue a cierto numero lo marque como zona peligrosa y le avise esto a la policia como urgente.
--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -3,14 +3,56 @@
 const formulario = document.querySelector('#contacto');
 const inputNombre = document.querySelector('#input-nombre');
 
+
+
+let nombrefinal = "";
+let comentariofinal = "";
+let newTitulo = "";
+let newComentario = "";
+let newP = null;
+let newH = null;
+
 function recibirNombre(event) {
   event.preventDefault();
-  console.log(event);
+  console.log("el formulario se ha enviado correctamente");
+  
+  //deposito la info en unas variables
+  nombrefinal = formulario[0].value;
+  comentariofinal = formulario[1].value;
+
+  //creo los atributos html
+  newH = document.createElement("H4");
+  newP = document.createElement("p");
+  
+  //paso a texto la info de las variables para poder utilizarlas
+  newTitulo = document.createTextNode(nombrefinal);
+  newComentario = document.createTextNode(comentariofinal);
+
+  //ingreso el texto dentro de los atributos html creados
+  newH.appendChild(newTitulo);
+  newP.appendChild(newComentario);
+
+  //ingreso los atributos con contenido dentro de un div ya anteriormente creado en el html
+  document.querySelector('.comentarios').appendChild(newH );
+  document.querySelector('.comentarios').appendChild(newP );
 }
 
+
+
 function interceptarNombre(event) {
-  console.log(event.target.value);
+  
+  let nombre = event.target.value;
+
+
+  if(nombre.length < 5 || nombre.length > 50){
+    console.log("El nombre debe ser entre 5 y 50 caracteres");
+  }else{
+    console.log("valor correcto");
+  }
 }
+
+
 
 formulario.addEventListener('submit', recibirNombre);
 inputNombre.addEventListener('input', interceptarNombre);
+
